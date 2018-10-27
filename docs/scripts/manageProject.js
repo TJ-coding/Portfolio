ced8f780bd6f1b8488c57e6b7eb9f86fd27f02f1
Tina3 <Tina3.com>
2018-10-27 18:57:49 +0100

    Added innter HTML feature

--- docs/scripts/manageProject.js
+++ docs/scripts/manageProject.js
@@ -6,10 +6,10 @@ function loadDoc() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      dealWithData(this.responseText);
-    }
-  };
-  xhttp.open("GET", DataUrl, true);
-  xhttp.send();
+   }
+ };
+ xhttp.open("GET", DataUrl, true);
+ xhttp.send();
 }
 
 function dealWithData(dataString){
@@ -32,22 +32,35 @@ function appendDrawLeaf(position,title,description,innerHtml,icon){
   //BE VERY CAREFUL LET NO ONE EDIT THE DATA BASE HTML!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   //IT IS VERY XSS ABLE AND IT IS INTEDED FOR IT TO BE POSSIBLE TO ALLOW FLEXIBILITY
   document.getElementById("horizontalPageWrapper").innerHTML+=`
-        <div class="horiontalPageElement">
-          <div style="padding-left:10vw;padding-top: 25vh;">
-            <table class="mainTitle" id="titleTable">
-              <tr><td width=30%"><img src="resource/icons/`+icon+`"
-                height="70em"
-                width="70em"
-                style="transform: rotate(90deg);"></td>
-                <td width="70%"><h1>`+title+`</h1></td><tr>
-                  <tr><td colspan="2">
-                    <p>`+description+`
-                    </p>
-                  </td></tr>
-                </table>
-              </div>
-            </div>
-          </div>`
+  <div class="horiontalPageElement">
+
+  <table>
+  <td >
+  <!--LeftHalf-->
+  <div style="padding-left:10vw;padding-top: 25vh;">
+  <table class="mainTitle" id="titleTable">
+  <tr><td width=30%"><img src="resource/icons/`+icon+`"
+  height="70em"
+  width="70em"
+  style="transform: rotate(90deg);"></td>
+  <td width="70%"><h1>`+title+`</h1></td><tr>
+  <tr><td colspan="2">
+  <p>`+description+`
+  </p>
+  </td></tr>
+  </table>
+  </div>
+  </div>
+  </td>
+  <!--RightHalf-->
+  <td style"padding-left: 7vw;vertical-align: top;padding-top: 10vh">
+  `+
+  innerHtml+`
+  </td>
+  </table>
+  </div>
+
+  `
 }
 //call flow
 //makeLeaves->loadDoc->dealWithData->appendDrawLeaf
