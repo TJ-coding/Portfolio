ea4873d07256a1e5a63b191ead5d04b1e57b66c0
Tina3 <Tina3.com>
2019-01-23 13:05:29 +0000

    Big changed (switching to bootstrap)
    A lot of changes were made to convert the page that use bootstrap

--- docs/scripts/manageProject2.js
+++ docs/scripts/manageProject2.js
@@ -21,31 +21,31 @@ function extractURL(dataString){
 //CALLED BY DEAL WITH DATA
 function findWhichDataToFetch(pageData){
   var description=pageData["description"];
-  var innerHTML=pageData["innerHTML"];
-  var whichDataToGet={"getDescription":false, "getInnerHTML":false}
+  var subTitle=pageData["subTitle"];
+  var whichDataToGet={"getDescription":false, "getSubTitle":false}
   whichDataToGet["getDescription"]=description.substring(0,16)=="!!!FetchData!!!:";
-  whichDataToGet["getInnerHTML"]=innerHTML.substring(0,16)=="!!!FetchData!!!:";
+  whichDataToGet["getSubTitle"]=subTitle.substring(0,16)=="!!!FetchData!!!:";
   return whichDataToGet
 }
 
-//fetch both description and innerHTML from URL then render the page
-function fetchDescriptInnerHTML(pageData){
+//fetch both description and subtitle from URL then render the page
+function fetchDescriptSubTitle(pageData){
     //extracting URL to fetch data from
     pageData.descriptionLocation=extractURL(pageData["description"])
-    pageData.innerHTMLLocation=extractURL(pageData["innerHTML"])
+    pageData.subTitleLocation=extractURL(pageData["subTitle"])
     //response function called after fetching description data
     var responseFunction=function(responseText,arrayArg){
       //responseText is the descriptionData
       arrayArg.description=responseText;
-      //response function called after fetching innerHTML data
+      //response function called after fetching subtitle data
       var secondResponseFunction=function(secondResponseText,arrayArg2){
         //secondResponseText is the innerHTML data
-        arrayArg2.innerHTML=secondResponseText;
+        arrayArg2.subTitle=secondResponseText;
         //send the paramters for page and render it
       renderPageObject.addPageToList(arrayArg2);
       }
-      //fetch InnerHTML
-      loadDoc(arrayArg["innerHTMLLocation"],secondResponseFunction,arrayArg)
+      //fetch subtitle
+      loadDoc(arrayArg["subTitleLocation"],secondResponseFunction,arrayArg)
     }
     //fetch Description
     loadDoc(pageData["descriptionLocation"],responseFunction,pageData)
@@ -63,15 +63,15 @@ function fetchDescription(pageData){
 
   }
 
-  function fetchInnerHTML(pageData){
+  function fetchSubTitle(pageData){
   //fetch innerHTML
-  pageData.innerHTMLLocation=extractURL(pageData["innerHTML"]);
+  pageData.subTitleLocation=extractURL(pageData["innerHTML"]);
   var responseFunction=function(responseText){
       //after fetching innerHTML
-      arrayArg.innerHTML=responseText;
+      arrayArg.subTitle=responseText;
       renderPageObject.addPageToList(arrayArg);
     }
-    loadDoc(pageData["innerHTMLLocation"],responseFunction,pageData)
+    loadDoc(pageData["subTitleLocation"],responseFunction,pageData)
   }
 
   //called by the main response function
@@ -85,17 +85,17 @@ function fetchDescription(pageData){
   for (var i = 0; i < numberOfProjects; i++) {
     //BE VERY CAREFUL LET NO ONE EDIT THE DATA BASE HTML!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     //IT IS VERY XSS ABLE AND IT IS INTEDED FOR IT TO BE POSSIBLE TO ALLOW FLEXIBILITY
-    var pageData = {"title":dataBase[i]["Title"],"description":dataBase[i]["Description"],"innerHTML":dataBase[i]["InnerHtml"],"icon":dataBase[i]["Icon"],"renderOrder":i};
+    var pageData = {"title":dataBase[i]["Title"],"description":dataBase[i]["Description"],"subTitle":dataBase[i]["SubTitle"],"icon":dataBase[i]["Icon"],"renderOrder":i};
     var whichDataToGet = findWhichDataToFetch(pageData);
-    //FETCH DESCRIPTION AND INNERHTML
-    if(whichDataToGet["getDescription"] && whichDataToGet["getInnerHTML"]){
-      fetchDescriptInnerHTML(pageData);
+    //FETCH DESCRIPTION AND SUBTITLE
+    if(whichDataToGet["getDescription"] && whichDataToGet["getSubTitle"]){
+      fetchDescriptSubTitle(pageData);
   //FETCH DESCRIPTION ONLY
 }else if(whichDataToGet["getDescription"]){
   fetchDescription(pageData)
-  //FETCH INNERHTML ONLY
-}else if(whichDataToGet["getInnerHTML"]){
-  fetchInnerHTML(pageData)
+  //FETCH subTitle ONLY
+}else if(whichDataToGet["getSubTitle"]){
+  fetchSubTitle(pageData)
   //FETCH NOTHING
 }else{
     //don't fetch anything
@@ -113,43 +113,57 @@ renderPageObject = {
     for (var i = 0; i<this.numberOfPages ; i++) {
       indexToRender=this.renderOrder[i];
       thisPageData=this.pageDataList[indexToRender];
-      this.appendPage(thisPageData["title"],thisPageData["description"],thisPageData["innerHTML"],thisPageData["icon"])
+      this.appendPage(thisPageData["title"],thisPageData["description"],thisPageData["subTitle"],thisPageData["icon"],i)
     }
   },
 
   //apend html to the horzontalPAgeWrapper
-  appendPage(title,description,innerHtml,icon){
+  appendPage(title,description,subTitle,icon,order){
  //BE VERY CAREFUL LET NO ONE EDIT THE DATA BASE HTML!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   //IT IS VERY XSS ABLE AND IT IS INTEDED FOR IT TO BE POSSIBLE TO ALLOW FLEXIBILITY
-  document.getElementById("horizontalPageWrapper").innerHTML+=`
-  <div class="horiontalPageElement">
-
-  <table>
-  <td >
-  <!--LeftHalf-->
-  <div style="padding-left:10vw;padding-top: 25vh;">
-  <table class="mainTitle" id="titleTable">
-  <tr><td width=30%"><img src="resource/icons/`+icon+`"
-  height="70em"
-  width="70em"></td>
-  <td width="70%"><h1>`+title+`</h1></td><tr>
-  <tr><td colspan="2">
-  <p>`+description+`
-  </p>
-  </td></tr>
-  </table>
-  </div>
-  </div>
-  </td>
-  <!--RightHalf-->
-  <td style"padding-left: 7vw;vertical-align: top;padding-top: 10vh">
-  `+
-  innerHtml+`
-  </td>
-  </table>
-  </div>
-
+  var elementToWriteIn=document.getElementById("carouselContainer")
+  carouselItem=`
+      <div class="carousel-item" style="height: 100vh; width: 100vw">
+        <div class="d-flex h-100 align-items-center justify-content-center projectSlide">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <div class="container-fluid d-md-flex align-items-center" style="max-width: 80%;">
+                    <div class="col-md-7 col-sm-12 text-md-right" style="text-align: center; height: 100%; ">
+                      <h1>`+title+`</h1>
+                    </div>
+                    <div class="col-md-5 col-sm-12 text-md-left" style="align-items: center;  justify-content:center;align-items:center;">
+                    <img src="resource/icons/`+icon+`"
+                    height="70em"
+                    width="70em"
+                    style="transform: rotate(90deg); margin-top: 10px">
+                  </div>
+              </div>
+            </div>
+          </div><br>
+          <hr>
+          <div class="row">
+            <div class="col">   
+              <div class="container" style="width: 60vw">
+                <div class="col-md-3 text-md-right">
+                  `+subTitle+`
+                </div>
+                <div class="col-md-9 text-md-left">
+                  <span>
+                    `+description+`
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   `
+  elementToWriteIn.innerHTML+=carouselItem
+  indicatorElement=document.getElementById("carouselIndicator")
+  dataToSlideTo=order+1
+  indicatorListTag='<li data-target="#projectCarousel" data-slide-to="'+dataToSlideTo+'"></li>'
+  indicatorElement.innerHTML+=indicatorListTag
   },
   //add page data to attribute
   addPageToList(pageData){
@@ -167,7 +181,7 @@ renderPageObject = {
 
 //call flow
 //makeLeaves->loadDoc->dealWithData->appendDrawLeaf
-function makeLeaves(){
+function makeCarouselPages(){
   var DataUrl="http://localhost/docs/projectDatabase/projectDatabase.html"
   //sending function dealWithData as response function
   loadDoc(DataUrl,dealWithDatabase,[])
